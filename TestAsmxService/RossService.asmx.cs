c25cb20823154378873faff1c2ba80a37f8f6b15
Rossall Sandford <rossall.sandford@ukho.gov.uk>
2019-05-30 16:05:51 +0100

    Initial commit

---
+++ TestAsmxService/RossService.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Services;
+
+namespace TestAsmxService
+{
+    /// <summary>
+    /// Summary description for RossService
+    /// </summary>
+    [WebService(Namespace = "http://rossall.dev/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class RossService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public string ReturnMessage(string toAppend) => $"You have provided {toAppend}";
+    }
+
+}
